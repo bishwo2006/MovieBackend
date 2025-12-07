6b1c5d37eb721a527f2155944b76d831ae6a2ff2
bishwo2006 <luitelbishwo106@gmail.com>
2025-12-07 16:04:10 +0545

    bkjk

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,6 @@ module.exports = async function connectDB() {
   const uri = process.env.MONGO_URI;
   if (!uri) throw new Error('MONGO_URI not set in env');
 
-  // OLD: causes error in Mongoose 7+
-  // await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
   // NEW: just pass uri
   await mongoose.connect(uri);
 
